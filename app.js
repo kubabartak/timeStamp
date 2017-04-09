2c0d14119637f3c5927ddf12b377fa0cefe2bb80
Jakub <kubabartak@yahoo.com>
2017-04-10 00:49:42 +0200

    add functionality

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -19,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+var months=["Januray", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+function fromUnix(unix){
+var date= new Date(unix*1000);
+var result= months[date.getMonth()] +" "+date.getDate()+", "+ date.getFullYear();
+return result;}
+function fromStr(str){
+    var date=new Date(str);
+    var result= months[date.getMonth()] +" "+date.getDate()+", "+ date.getFullYear();
+return result;
+}
 
+router.get("/:time", function(req, res){
+    if (!isNaN(req.params.time)) {
+        var natural=fromUnix(req.params.time)
+        data= {natural: natural,
+             unix: req.params.time };
+        res.json(data);
+    } else {
+        var natural =new Date(req.params.time);
+        if(!isNaN(natural)) {
+            var unix= natural/1000;
+            var result = fromStr(req.params.time);
+            data= {natural: result,
+             unix: unix};
+        res.json(data);
+        } else {
+            res.json({ unix: null, natural: null })
+        }
+    } 
+})
 module.exports = router;
